24ff7ad3bf504948bcb65532419bcf5f55ab29e8
diogoavila <aviladiogo30@live.com>
2021-09-02 18:54:02 -0300

    inserção de graficos

--- Ponto_Medio.py
+++ Ponto_Medio.py
@@ -1,9 +1,13 @@
+import matplotlib.pyplot as plt
+
 X1 = int(input("insira o valor de X1: "))
 Y1 = int(input("insira o valor de Y1: "))
 X2 = int(input("insira o valor de X2: "))
 Y2 = int(input("insira o valor de Y2: "))
 Pos1 = X1
 Pos2 = Y1
+x =[Pos1]
+y =[Pos2]
 print(str(Pos1) + ", " + str(Pos2))
 while Pos1 < X2:
     Dx = X2 - Pos1
@@ -14,16 +18,22 @@ while Pos1 < X2:
     Condição = (Dx*Pos1) + (-Dy*Pos2) + (Dy * B)
     
     if(Condição > 0):
-        print("NE" )
+    
         Pos1 += 1
         Pos2 +=1
     elif(Condição < 0):
-        print("E")
+        
         Pos1 += 1
 
     else:
-        print("o proximo pixel podera ser tanto em NE quanto E.")
+        
         Pos1 += 1
         Pos2 +=1
-    
+        x.append(Pos1)
+        y.append(Pos2)
     print(str(Pos1) + ", " + str(Pos2))
+plt.xlabel("X")
+plt.ylabel("Y2")
+
+plt.plot(x,y)
+plt.show()
